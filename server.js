b6c783bb0e5fc47c3033bd6b869309a114b9e1fb
Jesse <gumtow@gmail.com>
2020-05-06 17:38:52 -0500

    first commit

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const portfolioController = require('./controllers/portfolio.js');
-
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/portfolio';
 
 // Middleware
 app.use(express.static('public'))
@@ -17,12 +17,12 @@ app.use('/jgumtow', portfolioController);
 
 
 // Mongoose Connection
-mongoose.connect('mongodb://localhost:27017/portfolio', {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true, useUnifiedTopology:true});
 mongoose.connection.once('open', ()=>{
     console.log('connected to mongo');
 });
 
 // Listen on Port
-app.listen(port, ()=>{
-    console.log('Listening on port', port);
+app.listen(PORT, ()=>{
+    console.log('Listening on port', PORT);
 })
